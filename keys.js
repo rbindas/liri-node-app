455505121df822b9a9c9a7a989cb32de2196d493
rbindas <30477545+rbindas@users.noreply.github.com>
2017-12-27 11:24:34 -0500

    added twitterkeys to keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-	consumer_key: 'o6BxSYcNzPXMlZfqMt8LLKung',
-	consumer_secret: 'Chp7ouZBNIv7WQiMstd8hLN47ghd1IVsoA8Q8EN8PFwNZy6f6v',
-	access_token_key: '855459223404236801-YALG7YDKNRtePNmWM4wYQZ0g0jUnI1O',
-	access_token_secret: 'jxdkF6VawUC4LDpfmkRr0Egow0xMiKA1M5QgOX59HPlbQ'
+	consumer_key: 'a7xvuRRS4t5wOGnaFg56ronLX',
+	consumer_secret: 'bIypIENXe4XTieQ3aCPK2PdhMiRDy84qfULBnhoZHTQ0tBwX9m',
+	access_token_key: '946011302991982592-df1PlsyFKN2e36DJix18kSVLgVXfxS8',
+	access_token_secret: 'DRrQyvjOcYtN8wwcOyUi58bQFZilDfp9eIwVsh9vXZG08'
 }
