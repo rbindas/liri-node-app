80497290b8ccf1ea1a184d2a0cce5600914d3f5a
rbindas1 <rbindas1@gmail.com>
2017-12-26 19:29:34 -0500

    initial commit of files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: 'o6BxSYcNzPXMlZfqMt8LLKung',
+	consumer_secret: 'Chp7ouZBNIv7WQiMstd8hLN47ghd1IVsoA8Q8EN8PFwNZy6f6v',
+	access_token_key: '855459223404236801-YALG7YDKNRtePNmWM4wYQZ0g0jUnI1O',
+	access_token_secret: 'jxdkF6VawUC4LDpfmkRr0Egow0xMiKA1M5QgOX59HPlbQ'
+}
